85e14fe49f6eb610c537d6534d3c445821ab3f2a
Aparna Peri <aparna.alapana@gmail.com>
2026-06-19 01:10:15 +0800

    small changes made to dev

--- calculator.py
+++ calculator.py
@@ -11,6 +11,6 @@ class CalculatorPlus:
         return a * b
 
     def divide(self, a, b):
-        if b == 0:
-            raise ValueError("Cannot divide by zero.")
-        return a / b
+        if b == 0:  
+            raise ValueError("Cannot divide by zero.")  
+    	    return a / b
